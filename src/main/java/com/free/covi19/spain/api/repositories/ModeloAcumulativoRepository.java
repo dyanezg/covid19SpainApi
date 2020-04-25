6cc2908b13d4749e3b77e4630c40d8129ab36fa4
David Yáñez Garcia <dyanez@vectoritcgroup.com>
2020-04-26 01:13:56 +0200

    config

--- src/main/java/com/free/covi19/spain/api/repositories/ModeloAcumulativoRepository.java
+++ src/main/java/com/free/covi19/spain/api/repositories/ModeloAcumulativoRepository.java
@@ -1,11 +1,7 @@
 package com.free.covi19.spain.api.repositories;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.free.covi19.spain.api.model.ModeloAcumulativo;
 
@@ -13,9 +9,5 @@ import com.free.covi19.spain.api.model.ModeloAcumulativo;
 @RepositoryRestResource(collectionResourceRel = "modeloAcumulativo", path = "modeloAcumulativo")
 public interface ModeloAcumulativoRepository  extends CrudRepository<ModeloAcumulativo, Integer> {
 
-	
-	@Query("select m.id, m.comunidadAutonoma, m.fecha ,m.casos, m.hospitalizados, m.fechaImportacion from ModeloAcumulativo m")
-	@RestResource(path = "hospitalizados", rel = "hospitalizados")
-	List<ModeloAcumulativoRepository> getHospitalizados();
 	
 }
