412c4e1b89faa1606ff2decf2010b45265bec762
David Yáñez Garcia <dyanez@vectoritcgroup.com>
2020-05-07 13:06:43 +0200

    set time zone

--- src/main/java/com/free/covi19/spain/api/Covid19SpainApiApplication.java
+++ src/main/java/com/free/covi19/spain/api/Covid19SpainApiApplication.java
@@ -1,5 +1,9 @@
 package com.free.covi19.spain.api;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,6 +18,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAsync
 public class Covid19SpainApiApplication {
 
+	
+	  @PostConstruct
+	    void started() {
+	        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
+	    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Covid19SpainApiApplication.class, args);
 	}
